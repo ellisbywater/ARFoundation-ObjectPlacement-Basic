fa963b9f10e920b067e3bbd321059c545bb5bd3d
Ellis G <egaraudy@gmail.com>
2020-08-09 12:35:10 -0600

    create placement package

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    private PlacementIndicator _placementIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectToSpawn, _placementIndicator.transform.position,
+                _placementIndicator.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager _raycastManager;
+    private GameObject visual;
+
+    private void Start()
+    {
+        // get components
+        _raycastManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;    
+        
+        // placement indicator
+        visual.SetActive(false);
+    }
+
+    private void Update()
+    {
+        List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+        _raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), _hits, TrackableType.Planes);
+        
+        //Check if we hit AR plane, update position and rotation
+        if (_hits.Count > 0)
+        {
+            transform.position = _hits[0].pose.position;
+            transform.rotation = _hits[0].pose.rotation;
+            
+            if(!visual.activeInHierarchy)
+                visual.SetActive(true);
+        }
+    }
+}
